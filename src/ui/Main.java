df0f9b4bc938d6c03051a5bb4b400eb9be35780f
Santiago <santiesleo@gmail.com>
2022-10-31 14:37:38 -0500

    Primer prueba

---
+++ src/ui/Main.java
@@ -0,0 +1,13 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+    }
+
+}
